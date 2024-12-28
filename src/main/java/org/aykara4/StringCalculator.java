037516dac3115b04c50e8346b1a96a3798540543
augustineaykara <augustineaykara@gmail.com>
2024-12-28 12:19:58 +0530

    Refactored code and added comments for readability

--- src/main/java/org/aykara4/StringCalculator.java
+++ src/main/java/org/aykara4/StringCalculator.java
@@ -6,20 +6,29 @@ import java.util.List;
 public class StringCalculator {
 
     public int add(String numbers) {
+        // to handle empty string input case
         if(numbers.isEmpty()) {
             return 0;
         }
 
+        // default delimiter
         String delimiter = "[,\n]";
+
+        // to handle custom delimiter cases
         if(numbers.startsWith("//")) {
-            delimiter = numbers.substring(2, numbers.indexOf("\n"));
-            delimiter = "\\" + delimiter;
+            delimiter = "\\" + numbers.substring(2, numbers.indexOf("\n"));
             numbers = numbers.substring(numbers.indexOf("\n")+1);
         }
 
-        String[] integerArray = numbers.split(delimiter);
-        List<Integer> negativeList = new ArrayList<>();
+        // addition implementation
+        return calculate(numbers, delimiter);
+    }
+
+    private int calculate(String numbers, String delimiter) {
         int sum = 0;
+        List<Integer> negativeList = new ArrayList<>();
+        String[] integerArray = numbers.split(delimiter);
+
         for(String number : integerArray) {
             int num = Integer.parseInt(number);
             if(num < 0) {
@@ -27,6 +36,8 @@ public class StringCalculator {
             }
             sum += num;
         }
+
+        // to handle input with negative numbers
         if(!negativeList.isEmpty()) {
             throw new IllegalArgumentException("Negative numbers not allowed: " + negativeList);
         }
