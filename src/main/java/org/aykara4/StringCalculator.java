f5b33445f1f00abdfd057b5ee15e842efe009695
augustineaykara <augustineaykara@gmail.com>
2024-12-28 11:02:25 +0530

    Fixed multiple numbers with newline delimiter scenario

--- src/main/java/org/aykara4/StringCalculator.java
+++ src/main/java/org/aykara4/StringCalculator.java
@@ -5,19 +5,18 @@ public class StringCalculator {
     public int add(String numbers) {
         if(numbers.isEmpty()) {
             return 0;
-        } else if(numbers.contains(",")) {
-         String[] integerArray = numbers.split(",");
-         int sum = 0;
-         for(String num : integerArray) {
-             sum += Integer.parseInt(num);
-         }
-         return sum;
         }
-        int num = Integer.parseInt(numbers);
-        if(num < 0) {
-            throw new IllegalArgumentException("Negative numbers not allowed: " + num);
+
+        String[] integerArray = numbers.split("[,\n]");
+        int sum = 0;
+        for(String number : integerArray) {
+            int num = Integer.parseInt(number);
+            if(num < 0) {
+                throw new IllegalArgumentException("Negative numbers not allowed: " + num);
+            }
+            sum += num;
         }
-        return num;
+        return sum;
     }
 
 }
