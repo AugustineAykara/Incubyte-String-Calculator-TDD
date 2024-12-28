8472ea245aa1b3189873b7227b792b475b163948
augustineaykara <augustineaykara@gmail.com>
2024-12-28 10:47:11 +0530

    Fixed multiple number input scenario

--- src/main/java/org/aykara4/StringCalculator.java
+++ src/main/java/org/aykara4/StringCalculator.java
@@ -5,6 +5,13 @@ public class StringCalculator {
     public int add(String numbers) {
         if(numbers.isEmpty()) {
             return 0;
+        } else if(numbers.contains(",")) {
+         String[] integerArray = numbers.split(",");
+         int sum = 0;
+         for(String num : integerArray) {
+             sum += Integer.parseInt(num);
+         }
+         return sum;
         }
         int num = Integer.parseInt(numbers);
         if(num < 0) {
